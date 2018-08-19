c2e31a7f05de011ef45946f1d93e7d87c44ff344
Wayne Cook <WCook@superiorgrocers.com>
2018-08-18 22:05:47 -0700

    Initial

---
+++ autoloader.php
@@ -0,0 +1,31 @@
+<?php
+
+spl_autoload_register('autoload');
+
+function autoload( $class, $dir = null )
+{
+
+  //Set directory path
+  if ( is_null( $dir ) ) {
+    $dir = $_SERVER['DOCUMENT_ROOT'] . '/root/'; //Path to root
+  }
+
+  foreach ( scandir($dir) as $file ) {
+
+    //Full path to file
+    $filePath = $dir . $file;
+
+    //Is directory?
+    if ( is_dir( $filePath) && $file[0] !== '.' )  {
+      autoload($class, $filePath . '/');
+    }
+
+    // File info
+    $fileInfo = pathinfo($filePath);
+
+    //Include file if found
+    if ( $fileInfo['filename'] == $class && $fileInfo['extension'] == 'php' ) {
+      include $fileInfo['dirname'] . '/' . $fileInfo['basename'];
+    }
+  }
+}
