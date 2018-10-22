498a2fd71d292cf9c1733314514920fd0cd41c03
Wayne Cook <waynedemetra@gmail.com>
2018-10-22 12:32:51 -0700

    Update autoloader.php
    Added comment

--- autoloader.php
+++ autoloader.php
@@ -1,31 +1,32 @@
 <?php
 
-spl_autoload_register('autoload');
+spl_autoload_register( 'autoload' );
 
 function autoload( $class, $dir = null )
 {
 
   //Set directory path
   if ( is_null( $dir ) ) {
-    $dir = $_SERVER['DOCUMENT_ROOT'] . '/root/'; //Path to root
+    $dir = $_SERVER[ 'DOCUMENT_ROOT' ] . '/root/'; //change root path to your directory root
   }
 
-  foreach ( scandir($dir) as $file ) {
+  foreach ( scandir( $dir ) as $file ) {
 
     //Full path to file
     $filePath = $dir . $file;
 
     //Is directory?
-    if ( is_dir( $filePath) && $file[0] !== '.' )  {
-      autoload($class, $filePath . '/');
+    if ( is_dir( $filePath ) && $file[ 0 ] !== '.' )  {
+      autoload( $class, $filePath . '/' );
     }
 
     // File info
-    $fileInfo = pathinfo($filePath);
+    $fileInfo = pathinfo( $filePath );
 
     //Include file if found
-    if ( $fileInfo['filename'] == $class && $fileInfo['extension'] == 'php' ) {
-      include $fileInfo['dirname'] . '/' . $fileInfo['basename'];
+    if ( $fileInfo[ 'filename' ] == $class && $fileInfo[ 'extension' ] == 'php' ) {
+      include $fileInfo[ 'dirname' ] . '/' . $fileInfo[ 'basename' ];
     }
   }
 }
+
